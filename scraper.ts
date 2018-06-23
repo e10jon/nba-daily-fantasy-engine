653fe468b0f506f8e386816974a30e9a42824601
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-23 14:47:02 -0700

    scrape-all

--- scraper.ts
+++ scraper.ts
@@ -26,6 +26,7 @@ interface Row {
 class Scraper {
   private static NETWORK_ACRONYMS = ['dk', 'fd']
   private static SEASONS = {
+    2016: [new Date('2016-10-25'), new Date('2017-06-12')],
     2017: [new Date('2017-10-17'), new Date('2018-06-08')],
   }
 
@@ -142,5 +143,6 @@ class Scraper {
   }
 }
 
-const scraper = new Scraper(2017)
+const year = parseInt(process.argv[2] || '2017')
+const scraper = new Scraper(year)
 scraper.run().then(() => process.exit())
