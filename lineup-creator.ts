8c5bb2f0d6bacf2945b43d9b4e0b6c6294083636
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-26 15:37:48 -0700

    generating lineups

--- lineup-creator.ts
+++ lineup-creator.ts
@@ -1,10 +1,15 @@
 import * as _compact from 'lodash/compact'
+import * as _concat from 'lodash/concat'
 import * as _map from 'lodash/map'
+import * as _remove from 'lodash/remove'
+import * as _sample from 'lodash/sample'
+import * as _shuffle from 'lodash/shuffle'
 import * as _sum from 'lodash/sum'
 
 import {getColumnPrefix, statsTable} from './db'
 import Networks from './networks'
 import Strategies from './strategies';
+import { fstat } from 'fs';
 
 class Player {
   playerId: number
@@ -30,27 +35,28 @@ class Lineup {
   sf2: Player
   pf1: Player
   pf2: Player
-  c: Player
-  u: Player
+  c1: Player
+  g1: Player
+  f1: Player
+  u1: Player
 
   constructor (network: number) {
     this.network = network
   }
 
   isValid = () => this.isFilled() && this.isUnderSalaryCap()
+  isUnderSalaryCap = () => _sum(_map(this.players(), 'salary')) <= this.salaryCap()
 
-  players = () => _compact([this.pg1, this.pg2, this.sg1, this.sg2, this.sf1, this.sf2, this.pf1, this.pg2, this.c, this.u])
+  players = () => _compact([this.pg1, this.pg2, this.sg1, this.sg2, this.sf1, this.sf2, this.pf1, this.pg2, this.c1, this.g1, this.f1, this.u1])
 
   private isFilled = () => {
     switch (this.network) {
       case Networks.DraftKings:
-        return this.pg1 && this.sg1 && this.sf1 && this.pf1 && this.c && this.u && (this.pg2 || this.sg2) && (this.sf2 || this.pf2)
+        return this.pg1 && this.sg1 && this.sf1 && this.pf1 && this.c1 && this.u1 && this.g1 && this.f1
       case Networks.FanDuel:
-        return this.pg1 && this.pg2 && this.sg1 && this.sg2 && this.sf1 && this.sf2 && this.pf1 && this.pf2 && this.c
+        return this.pg1 && this.pg2 && this.sg1 && this.sg2 && this.sf1 && this.sf2 && this.pf1 && this.pf2 && this.c1
     }
   }
-
-  private isUnderSalaryCap = () => _sum(_map(this.players(), 'salary')) <= this.salaryCap()
 
   private salaryCap = () => {
     switch (this.network) {
@@ -87,6 +93,72 @@ export default class LineupCreator {
       }
       return arr
     }, [])
+  }
+
+  generateLineup = () => {
+    const lineup = new Lineup(this.network)
+
+    let pgs = []
+    let sgs = []
+    let sfs = []
+    let pfs = []
+    let cs = []
+
+    for (const player of this.pool) {
+      if (/\bPG\b/.test(player.position)) pgs.push(player)
+      if (/\bSG\b/.test(player.position)) sgs.push(player)
+      if (/\bSF\b/.test(player.position)) sfs.push(player)
+      if (/\bPF\b/.test(player.position)) pfs.push(player)
+      if (/\bC\b/.test(player.position)) cs.push(player)
+    }
+
+    const spots = (() => {
+      switch (this.network) {
+        case Networks.DraftKings: return ['pg1', 'sg1', 'sf1', 'pf1', 'c1', 'g1', 'f1', 'u1']
+        case Networks.FanDuel: return ['pg1', 'pg2', 'sg1', 'sg2', 'sf1', 'sf2', 'pf1', 'pf2', 'c1']
+      }
+    })()
+
+    for (const spot of _shuffle(spots)) {
+      const subpool = (() => {
+        switch (spot) {
+          case 'pg1':
+          case 'pg2':
+            return pgs
+          case 'sg1':
+          case 'sg2':
+            return sgs
+          case 'sf1':
+          case 'sf2':
+            return sfs
+          case 'pf1':
+          case 'pf2':
+            return pfs
+          case 'c1':
+            return cs
+          case 'g1':
+            return _concat(pgs, sgs)
+          case 'f1':
+            return _concat(sfs, pfs)
+          case 'u1':
+            return _concat(pgs, sgs, sfs, pfs, cs)
+        }
+      })()
+
+      let player
+
+      do {
+        player = _sample(subpool)
+        lineup[spot] = player
+      } while (!lineup.isUnderSalaryCap())
+
+      // remove the player from all pools so he can't be picked again
+      for (let s of [pgs, sgs, sfs, pfs, cs]) {
+        _remove(s, p => p.playerId === player.playerId)
+      }
+    }
+
+    return lineup
   }
 
   private createValue = (row, {columnPrefix}) => {
--- test/lineup-creator.spec.ts
+++ test/lineup-creator.spec.ts
@@ -4,10 +4,19 @@ import LineupCreator from '../lineup-creator'
 import Networks from '../networks'
 import Strategies from '../strategies'
 
+const TEST_DATE = '2016-10-25'
+
 describe('LineupCreator', () => {
-  it('generates lineups based on actual points', async () => {
-    const lineup = new LineupCreator('2016-10-25', Networks.FanDuel, Strategies.Actual)
-    await lineup.fillPool()
-    expect(lineup.pool.length).toBe(70)
+  describe('#generateLineup', () => {
+    for (let network of [Networks.DraftKings, Networks.FanDuel]) {
+      for (let strategy of [Strategies.Actual]) {
+        it(`${Networks[network]} ${Strategies[strategy]}`, async () => {
+          const lineupCreator = new LineupCreator(TEST_DATE, network, strategy)
+          await lineupCreator.fillPool()
+          const lineup = lineupCreator.generateLineup()
+          expect(lineup.isValid()).toBeTruthy()
+        })
+      }
+    }
   })
 })
